5674b657120d2921ce9ddaabbd91e324b1feb6e7
chris <chris@bumblehead.com>
2017-01-13 00:40:16 -0800

    added number casting. added cast method names corresponding to typeof types

--- castas.js
+++ castas.js
@@ -1,41 +1,62 @@
-var castas = module.exports = (function (o) {
+// Filename: castas.js  
+// Timestamp: 2017.01.13-00:27:11 (last modified)
+// Author(s): bumblehead <chris@bumblehead.com>
 
-  o.bool = function (val, defval) {
-    if (String(val) === 'true')  defval = true;
-    if (String(val) === 'false') defval = false;    
+const castas = module.exports = (o => {
+
+  o.bool = (val, defval) => {
+    if (String(val) === 'true')
+      defval = true;
+    else if (String(val) === 'false')
+      defval = false;    
 
     return Boolean(defval);    
   };
 
-  o.arr = function (val, defval) {
-    if (Array.isArray(val))      defval = val;
-    if (typeof val === 'string') defval = val.split(',');
+  o.arr = (val, defval) => {
+    if (Array.isArray(val))
+      defval = val;
+    else if (typeof val === 'string')
+      defval = val.split(',');
 
     return defval;    
   };
 
-  o.str = function (val, defval) {
-    if (/string|number|boolean/
-        .test(typeof val)) defval = val;
-    
-    return String(defval);
-  };
-
-  o.ts = function (val, defval) {
-    if (!isNaN(parseFloat(val)) && isFinite(val)) val = +val;
-    if (val instanceof Date || (
-        /string|number/
-        .test(typeof val))) defval = (new Date(val)).getTime();
+  o.num = (val, defval) => {
+    if (typeof val === 'number')
+      defval = val;
+    else if (!isNaN(parseFloat(val)) && isFinite(val))
+      defval = +val;
 
     return defval;
   };
 
-  Object.keys(o).forEach(function (fnname) {
-    o['t'+fnname] = function (val, defval) {
-      return o[fnname](val || null, defval);
-    };
-  });
+  o.str = (val, defval) => {
+    if (/string|number|boolean/.test(typeof val))
+      defval = val;
+    
+    return String(defval);
+  };
+
+  o.ts = (val, defval) => {
+    if (!isNaN(parseFloat(val)) && isFinite(val)) val = +val;
+    if (val instanceof Date || (
+        /string|number/.test(typeof val)))
+      defval = (new Date(val)).getTime();
+
+    return defval;
+  };
+
+  
+  o.boolean = o.bool;
+  o.number = o.num;
+  o.string = o.str;
+
+  Object.keys(o).forEach(fnname => (
+    o['t'+fnname] = (val, defval) => 
+      o[fnname](val || null, defval)
+  ));
 
   return o;
   
-}({}));
+})({});
